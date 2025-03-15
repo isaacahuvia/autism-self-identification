e30fe244be6529545dbf4aedfb2de08c15cf5143
Isaac Ahuvia <isaacahuvia@gmail.com>
2025-03-15 11:40:59 -0400

    finalize filtering steps, recoding; merge in qual data

--- 3_Clean Data.R
+++ 3_Clean Data.R
@@ -5,6 +5,9 @@
 ## Startup
 library(tidyverse)
 library(qualtRics)
+library(openxlsx)
+library(scales)
+library(here)
 `%+%` <- paste0
 
 
@@ -12,18 +15,25 @@ library(qualtRics)
 # Raw data (see 1_Pull Raw Data.R)
 raw_data <- readRDS("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Qualtrics Raw Data.rds")
 
-# Prolific IDs (for deduplication)
-prolific_ids <- bind_rows(
+# Approved prolific IDs (for filtering and deduplication)
+approved_prolific_ids <- bind_rows(
   read.csv("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Prolific Demographics\\prolific_export_654d2307ccdc612e1c3652de.csv"),
   read.csv("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Prolific Demographics\\prolific_export_654d22ce099cb0a0fd753451.csv")
 ) %>%
-  select(
-    prolific_id = Participant.id, 
-    status = Status
-  )
+  filter(Status == "APPROVED") %>%
+  pull(Participant.id)
 
-# Coded qualitative data (WIP)
-
+# Coded qualitative data
+q1 <- read.xlsx("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Coded Qualitative Responses\\Q1.xlsx") %>%
+  select(response_id, starts_with("q1"))
+q2 <- read.xlsx("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Coded Qualitative Responses\\Q2.xlsx") %>%
+  select(response_id, starts_with("q2"))
+q3 <- read.xlsx("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Coded Qualitative Responses\\Q3.xlsx") %>%
+  select(response_id, starts_with("q3"))
+q4 <- read.xlsx("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Coded Qualitative Responses\\Q4.xlsx") %>%
+  select(response_id, starts_with("q4"))
+q5 <- read.xlsx("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Coded Qualitative Responses\\Q5.xlsx") %>%
+  select(response_id, starts_with("q5"))
 
 # Lookup table for manually recoding the variable age_first_aware
 age_first_aware_recoding <- read.csv("H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Manual Recode Tables\\Age First Aware Recode.csv",
@@ -32,39 +42,61 @@ age_first_aware_recoding <- read.csv("H:\\My Drive\\Research\\Projects\\Autism I
 
 
 ####  Clean Data  ####
-## Limit to those who consent
-consenters <- raw_data %>%
+## Filter responses
+# Check rows in raw data
+nrow(raw_data)
+
+# Filter to those who passed both screeners
+screened_respondents <- raw_data %>%
+  filter(
+    prolific_screener %in% c(
+      "Yes - as a child",
+      "Yes - as an adult",
+      "No - but I identify as being on the autism spectrum",
+      "I am in the process of receiving a diagnosis"
+    ),
+    identity_screener %in% c(
+      "Yes, I identify as autistic or on the autism spectrum",
+      "I have a different identity, diagnosis, or condition on the autism spectrum"
+    )
+  )
+
+nrow(screened_respondents)
+
+# Filter to those who provided consent
+consenting_respondents <- screened_respondents %>%
   filter(consent %in% "I agree to participate in this study")
 
+nrow(consenting_respondents)
 
-## Limit to participants with valid Prolific IDs
-consenters_with_prolific_ids <- consenters %>%
-  left_join(prolific_ids, by = "prolific_id") %>%
-  drop_na(prolific_id)
+# Filter to approved (i.e., paid) participant IDs
+approved_respondents <- consenting_respondents %>%
+  filter(prolific_id %in% approved_prolific_ids)
+
+nrow(approved_respondents)
+
+# Filter to non-duplicated responses
+unique_respondents <- approved_respondents %>%
+  group_by(prolific_id) %>%
+  arrange(desc(Finished)) %>%
+  slice_head(n = 1) %>%
+  ungroup()
+
+nrow(unique_respondents)
+
+# Filter to high-attention responses
+high_attention_respondents <- unique_respondents %>%
+  mutate(
+    age_implied_by_dob = 2023 - dob,
+    age_difference = abs(age - age_implied_by_dob)
+  ) %>%
+  filter(age_difference <= 1)
+
+nrow(high_attention_respondents)
 
 
-## Limit to non-duplicated participants
-duplicated_prolific_ids <- consenters_with_prolific_ids %>%
-  drop_na(prolific_id) %>%
-  filter(duplicated(prolific_id)) %>%
-  pull(prolific_id)
-
-# Identify duplicate
-consenters_with_prolific_ids %>%
-  filter(prolific_id %in% duplicated_prolific_ids) # Two rows from one responder
-
-# Manually grab the repsonse ID of the incomplete response, to filter out
-duplicated_id <- consenters_with_prolific_ids %>%
-  filter(prolific_id %in% duplicated_prolific_ids,
-         Finished == F) %>%
-  pull(ResponseId)
-
-# Remove duplicate
-nonduplicates <- consenters_with_prolific_ids %>%
-  filter(ResponseId != duplicated_id)
-
-
-## Manually correct "problems" in speaking variables
+## Recode variables
+# Manually correct "problems" in speaking variables
 # fetch_survey() expects a trailing space for some levels and therefore sets them
 # as NA. Luckily, parse_factor() tells us when and where this happens so we can correct
 for(x in c("speaking_child", "speaking_child_AAC", "speaking_now", "speaking_now_AAC")) {
@@ -81,14 +113,8 @@ for(x in c("speaking_child", "speaking_child_AAC", "speaking_now", "speaking_now
   
 }
 
-
-## Manually correct fishy values
-# 65425c72d94cab4a2b4caae4 listed their age as 5; set to NA
-nonduplicates$age[nonduplicates$prolific_id == "65425c72d94cab4a2b4caae4"] <- NA
-
-
-## Recode variables
-recoded_data <- nonduplicates %>%
+# Recode remaining variables
+recoded_data <- high_attention_respondents %>%
   
   left_join(age_first_aware_recoding, by = "aware_when") %>%
   
@@ -98,6 +124,11 @@ recoded_data <- nonduplicates %>%
     ### Self-identification
     ## Self-identification, current
     self_id = dx == "No, I have not received a formal diagnosis",
+    self_id_char = case_match(
+      self_id, 
+      T ~ "Self-Identifying",
+      F ~ "Diagnosed"
+    ),
     
     ## Self-identification, ever
     self_id_ever = self_id | id_before_dx %in% "Yes",
@@ -105,7 +136,7 @@ recoded_data <- nonduplicates %>%
     
     ### Demographic factors
     ## Age: Good as-is
-    
+
     ## Gender
     gender = case_when(
       gender_3 == "Non-binary"
@@ -122,6 +153,15 @@ recoded_data <- nonduplicates %>%
       T ~ NA_character_
     ),
     
+    across(
+      all_of("gender_" %+% 1:12),
+      ~ case_when(
+        !is.na(.) ~ .,
+        is.na(.) & !is.na(gender) ~ "Not Selected",
+        is.na(.) & is.na(gender) ~ NA_character_
+      )
+    ),
+    
     ## Sexuality
     sexuality = case_when(
       sexuality_2 == "Gay, Lesbian, or Homosexual"
@@ -135,6 +175,15 @@ recoded_data <- nonduplicates %>%
       | sexuality_10 == "A sexual orientation not listed here" ~ "SM",
       sexuality_1 == "Straight or Heterosexual" ~ "Straight",
       T ~ NA_character_
+    ),
+    
+    across(
+      all_of("sexuality_" %+% 1:10),
+      ~ case_when(
+        !is.na(.) ~ .,
+        is.na(.) & !is.na(sexuality) ~ "Not Selected",
+        is.na(.) & is.na(sexuality) ~ NA_character_
+      )
     ),
     
     ## Race/ethnicity
@@ -158,7 +207,21 @@ recoded_data <- nonduplicates %>%
       T ~ NA_character_
     ),
     
-    ## Education: Good as-is
+    race_ethnicity_bin = race_ethnicity == "White non-Hispanic",
+    
+    ## Education
+    education = as.character(education),
+    
+    education_ord = case_match(
+      education,
+      c("Less than a high school diploma", "High school degree or equivalent (e.g., GED, modified diploma, certificate of completion)") ~ 1,
+      "Some college" ~ 2,
+      "Associate degree (e.g., AA, AS)" ~ 3,
+      "Bachelor’s degree (e.g., BA, BS)" ~ 4,
+      c("Master’s degree (e.g., MA, MS)", "Professional or doctorate degree (e.g., MD, DDS, DVM, PhD, EdD, PsyD)") ~ 5,
+       "Other:" ~ NA_real_
+    ),
+    
     
     ## Employment (NOTE: Differs from pre-registration; better information here)
     # Use of %in% treats missing values (NA) as FALSE. Appropriate for select boxes
@@ -239,10 +302,12 @@ recoded_data <- nonduplicates %>%
       )
     ),
     
-    ## Disability: Good as-is
-    
-    ## Autism as disability
-    disability_autism = grepl("autism", disability_which, ignore.case = T),
+    ## Disability
+    disability = if_else(
+      disability %in% c("Yes", "No", "Unsure"),
+      as.character(disability),
+      NA_character_
+    ),
     
     ## Impairment (WHODAS)
     across(
@@ -320,15 +385,15 @@ recoded_data <- nonduplicates %>%
     ### Beliefs about autism
     ## Autism definition: Good as-is (autism_def)
     
-    ## Autism as disability: Good as-is (autism_dis)
-    
+    ## Autism as disability
+    autism_dis = as.character(autism_dis),
     
     ### Other aspects
     ## Age first aware
     age_first_aware = aware_when_clean,
     
     ## Age first diagnosis
-    age_first_dx = as.numeric(dx_when),
+    age_first_dx = as.numeric(dx_when), # One coerced NA value (that's ok - this wasn't an age)
     
     ## Family/friends with diagnosis
     fam_dx = famfriend_dx_1 %in% "Yes, family (Optional: What is their relationship to you?)",
@@ -448,17 +513,48 @@ recoded_data <- nonduplicates %>%
 ## Select variables for analyses
 selected_data <- recoded_data %>%
   select(response_id = ResponseId, 
-         self_id, self_id_ever, age, starts_with("gender"), starts_with("sexuality"), 
-         race_ethnicity, education, employment_employed:employment_unable, cati_soc, 
-         cati_com, cati_cam, cati_rig, cati_rep, cati_sen, cati_sum, raads_sum, raads_cutoff, 
+         self_id, self_id_char, self_id_ever, age, starts_with("gender"), starts_with("sexuality"), 
+         race_ethnicity, race_ethnicity_bin, education, education_ord, employment_employed:employment_unable, 
+         cati_soc, cati_com, cati_cam, cati_rig, cati_rep, cati_sen, cati_sum, raads_sum, raads_cutoff, 
          speaking_now, speaking_child, phq_dep, phq_anx, disability, whodas_sum, 
          wemwbs_sum, asis_solidarity, asis_satisfaction, asis_centrality, asis_self_stereotyping, 
          asis_in_group_homogeneity, asis_self_investment, asis_self_definition, asis_sum, 
          starts_with("support_"), starts_with("autism_"),
          age_first_aware, age_first_dx, fam_dx, friend_dx, famfriend_dx, fam_id, friend_id, 
-         famfriend_id, nodx_want, disability_autism)
+         famfriend_id, nodx_want, access, understand)
 
 
+## Merge in qualitative data
+# ID Overlap
+id_overlap <- function(x, y) {
+  
+  if(any(duplicated(x))) stop("x should not have duplicates")
+  if(any(duplicated(y))) stop("y should not have duplicates")
+  
+  if(any(is.na(x))) stop("x should not have NAs")
+  if(any(is.na(y))) stop("y should not have NAs")
+  
+  x_in_y <- mean(x %in% y)
+  y_in_x <- mean(y %in% x)
+  
+  print(percent(x_in_y) %+% " of IDs of x in y")
+  print(percent(y_in_x) %+% " of IDs of y in x")
+  
+}
 
-## Save complete and pre-registered datasets
-saveRDS(selected_data, "H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Analysis-Ready Data.rds")
+id_overlap(selected_data$response_id, q1$response_id)
+id_overlap(selected_data$response_id, q2$response_id)
+id_overlap(selected_data$response_id, q3$response_id)
+id_overlap(selected_data$response_id, q4$response_id)
+id_overlap(selected_data$response_id, q5$response_id)
+
+merged_data <- selected_data %>%
+  left_join(q1, by = "response_id", relationship = "one-to-one") %>%
+  left_join(q2, by = "response_id", relationship = "one-to-one") %>%
+  left_join(q3, by = "response_id", relationship = "one-to-one") %>%
+  left_join(q4, by = "response_id", relationship = "one-to-one") %>%
+  left_join(q5, by = "response_id", relationship = "one-to-one")
+  
+
+## Save complete and pre-registered dataset
+saveRDS(merged_data, "H:\\My Drive\\Research\\Projects\\Autism Identification\\Data\\Analysis-Ready Data.rds")
